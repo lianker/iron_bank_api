810aec5c245b374c4e854050ca5ed27a97f678fc
lianker lopes <lianker.dev@gmail.com>
2019-11-12 15:43:18 -0300

    Add services folder and rspec to tests
    Create a services folder to store Business Logic, this will make tests
    and code structure more easy.

---
+++ app/services/operations_manager.rb
@@ -0,0 +1,3 @@
+class OperationsManager
+  
+end
---
+++ spec/services/operations_manager_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe OperationsManager do
+  subject { OperationsManager.new }
+  it 'should be true' do
+  end
+end
