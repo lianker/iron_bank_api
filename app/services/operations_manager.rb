dd4bdafce357167f29fa8923e53e847b6ccb53f4
lianker lopes <lianker.dev@gmail.com>
2019-11-12 21:22:49 -0300

    Add check_balance method in OperationsManager
    This function returns a hash containing te balance from an account.
    
    params: account_number
    Returns:
    	success case: {data: { balance: number }
    	error case: { error: bool, data: string }
    
    error: present in error cases like account not found
    data: returns message errors or hash with balance value

--- app/services/operations_manager.rb
+++ app/services/operations_manager.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 class OperationsManager
-  
+  def initialize; end
+
+  def check_balance(account_number:)
+    account = Account.find_by(number: account_number)
+    return { error: true, data: 'account not found' } if account.nil?
+
+    transactions = Transaction.find_by(account_id: account.id)
+    return { data: { balance: 0.00 } } if transactions.nil?
+
+    balance = transactions.inject(0) { |sum, item| sum + item.ammount }
+
+    { data: { balance: balance } }
+  end
 end
--- spec/services/operations_manager_spec.rb
+++ spec/services/operations_manager_spec.rb
@@ -4,6 +4,79 @@ require 'rails_helper'
 
 describe OperationsManager do
   subject { OperationsManager.new }
-  it 'should be true' do
+
+  describe '#check_balance(account_number)' do
+    context 'account not found' do
+      let(:invalid_number) { '1964' }
+
+      it 'should call Account find_by with account_number' do
+        expect(Account)
+          .to receive(:find_by).with(number: '1964').and_call_original
+
+        subject.check_balance(account_number: invalid_number)
+      end
+
+      it 'should returns hash with error key if account nil' do
+        allow(Account).to receive(:find_by)
+          .with(number: invalid_number).and_return(nil)
+
+        expect(subject.check_balance(account_number: invalid_number))
+          .to include(error: true)
+      end
+
+      it "should returns hash with key data 'account not found' if account nil" do
+        allow(Account).to receive(:find_by)
+          .with(number: invalid_number).and_return(nil)
+
+        expect(subject.check_balance(account_number: invalid_number))
+          .to include(data: 'account not found')
+      end
+    end
+
+    context 'accounts exists' do
+      let(:account_id) { 5 }
+
+      before(:each) do
+        acc_double = double
+        allow(acc_double).to receive(:id).and_return(account_id)
+
+        allow(Account).to receive(:find_by)
+          .with(number: '1964').and_return(acc_double)
+      end
+
+      it 'should call Transaction.find_by(account_id:?) method' do
+        expect(Transaction).to receive(:find_by)
+          .with(account_id: account_id).and_return(nil).and_call_original
+
+        subject.check_balance(account_number: '1964')
+      end
+
+      it 'if transactions nil returns data with key named balance=0.00' do
+        allow(Transaction).to receive(:find_by)
+          .with(account_id: account_id).and_return(nil)
+        result = subject.check_balance(account_number: '1964')
+        expect(result[:data])
+          .to include(balance: 0.00)
+      end
+
+      it 'returns the sum of transaction quantities as balance' do
+        transactions = [double, double]
+        transactions.each do |tr|
+          val = rand(100.0..1000.0).round(2)
+          allow(tr).to receive(:ammount)
+            .and_return(val)
+        end
+
+        allow(Transaction).to receive(:find_by)
+          .with(account_id: account_id).and_return(transactions)
+
+        expected = transactions[0].ammount + transactions[1].ammount
+
+        result = subject.check_balance(account_number: '1964')
+
+        expect(result[:data])
+          .to include(balance: expected)
+      end
+    end
   end
 end
