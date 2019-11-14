d5323a5b144413c1fa8216235de6fa18ede994b1
lianker lopes <lianker.dev@gmail.com>
2019-11-13 22:50:03 -0300

    add token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@
 
 class User < ApplicationRecord
   has_one :account
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = Digest::SHA1.hexdigest([Time.now, rand].join)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_035046) do
+ActiveRecord::Schema.define(version: 2019_11_14_014351) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "number"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_035046) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
   end
 
   add_foreign_key "accounts", "users"
